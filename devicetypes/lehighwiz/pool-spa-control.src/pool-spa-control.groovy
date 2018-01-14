b55b86717074a192b8aac763844df4cbbe5fb39d
Matthew Brennan <matthew.brennan@me.com>
2018-01-14 13:17:22 -0600

    Initial Commit

--- devicetypes/lehighwiz/pool-spa-control.src/pool-spa-control.groovy
+++ devicetypes/lehighwiz/pool-spa-control.src/pool-spa-control.groovy
@@ -1,7 +1,7 @@
 /**
- *   Pool/Spa Control
+ *  Pool/Spa: Generic Controller
  *
- *  Copyright 2017 Matthew Brennan
+ *  Copyright 2018 Matthew Brennan 
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -14,113 +14,58 @@
  *
  */
 metadata {
-	definition (name: " Pool/Spa Control", namespace: "lehighwiz", author: "Matthew Brennan") {
-		capability "Button"
-		capability "Temperature Measurement"
-		capability "Thermostat Fan Mode"
-		capability "Thermostat Heating Setpoint"
-		capability "Thermostat Operating State"
-		capability "Valve"
+	definition (name: "Pool/Spa: Control", namespace: "lehighwiz", author: "Matthew Brennan") {
+		capability "Switch"
+		capability "Refresh"
+        command "Control"
+        command "QueryStringParam"
+        
+    
+
+	tiles {
+		// TODO: define your main and details tiles here
+        standardTile("switch", "device.switch", width: 2, height: 2, decoration: "flat", canChangeIcon: true, canChangeBackground: true) {
+		state "On", label: 'On', action: "switch.off", icon: "st.Lighting.light11", backgroundColor: "#00a0dc", nextState:"Off"
+		state "Off", label: 'Off', action: "switch.on", icon: "st.Lighting.light13", backgroundColor: "#ffffff", nextState:"On"
+		}
 	}
-
-
-	simulator {
-		// TODO: define status and reply messages here
-	}
-
-	  tiles(scale: 2) {
-    multiAttributeTile(name:"partition", type: "generic", width: 6, height: 4) {
-      tileAttribute ("device.dscpartition", key: "PRIMARY_CONTROL") {
-        attributeState "ready", label: 'Ready', icon:"st.Home.home2"
-        attributeState "notready", label: 'Not Ready', backgroundColor: "#ffcc00", icon:"st.Home.home2"
-        attributeState "arming", label: 'Arming', backgroundColor: "#ffcc00", icon:"st.Home.home3"
-        attributeState "armedstay", label: 'Armed Stay', backgroundColor: "#79b821", icon:"st.Home.home3"
-        attributeState "armedaway", label: 'Armed Away', backgroundColor: "#79b821", icon:"st.Home.home3"
-        attributeState "armedinstant", label: 'Armed Instant Stay', backgroundColor: "#79b821", icon:"st.Home.home3"
-        attributeState "armedmax", label: 'Armed Instant Away', backgroundColor: "#79b821", icon:"st.Home.home3"
-        attributeState "alarmcleared", label: 'Alarm in Memory', backgroundColor: "#ffcc00", icon:"st.Home.home2"
-        attributeState "alarm", label: 'Alarm', backgroundColor: "#ff0000", icon:"st.Home.home3"
-      }
-      tileAttribute ("panelStatus", key: "SECONDARY_CONTROL") {
-        attributeState "panelStatus", label:'${currentValue}'
-      }
-    }
-
-    standardTile("armAwayButton","device.button", width: 2, height: 2, canChangeIcon: true, decoration: "flat") {
-      state "default", label: 'Away', action: "armAway", icon: "st.Home.home3", backgroundColor: "#79b821"
-    }
-
-    standardTile("armStayButton","device.button", width: 2, height: 2, canChangeIcon: true, decoration: "flat") {
-      state "default", label: 'Stay', action: "armStay", icon: "st.Home.home3", backgroundColor: "#79b821"
-    }
-
-    standardTile("disarmButton","device.button", width: 2, height: 2, canChangeIcon: true, decoration: "flat") {
-      state "default", label: 'Disarm', action: "disarm", icon: "st.Home.home2", backgroundColor: "#C0C0C0"
-    }
-
-    standardTile("armInstantButton","device.button", width: 2, height: 2, canChangeIcon: true, decoration: "flat") {
-      state "default", label: 'Instant', action: "armInstant", icon: "st.Home.home3", backgroundColor: "#79b821"
-    }
-
-    standardTile("chimeButton","device.button", width: 2, height: 2, canChangeIcon: true, decoration: "flat") {
-      state "default", label: 'Chime', action: "chime", icon: "st.custom.sonos.unmuted", backgroundColor: "#79b821"
-    }
-
-    main "partition"
-
-    details(["partition",
-             "armAwayButton", "armStayButton", "disarmButton",
-             "armInstantButton", "chimeButton"])
-  }
-
 }
+}
+
 
 // parse events into attributes
 def parse(String description) {
 	log.debug "Parsing '${description}'"
-	// TODO: handle 'button' attribute
-	// TODO: handle 'numberOfButtons' attribute
-	// TODO: handle 'temperature' attribute
-	// TODO: handle 'thermostatFanMode' attribute
-	// TODO: handle 'heatingSetpoint' attribute
-	// TODO: handle 'thermostatOperatingState' attribute
-	// TODO: handle 'contact' attribute
-	// TODO: handle 'valve' attribute
+	// TODO: handle 'switch' attribute
 
 }
 
 // handle commands
-def fanOn() {
-	log.debug "Executing 'fanOn'"
-	// TODO: handle 'fanOn' command
+def Control(String state, String stateDesc) {
+sendEvent (name: "switch", value: "${state}", descriptionText: "State passively changed to: ${stateDesc}")
 }
 
-def fanAuto() {
-	log.debug "Executing 'fanAuto'"
-	// TODO: handle 'fanAuto' command
+def QueryStringParam(String param, String switchLabel1, String switchLabel2, String switchValue1, String switchValue2) {
+state.qStringParam=param
+state.switchLabel1=switchLabel1
+state.switchLabel2=switchLabel2
+state.switchValue1=switchValue1
+state.switchValue2=switchValue2
 }
 
-def fanCirculate() {
-	log.debug "Executing 'fanCirculate'"
-	// TODO: handle 'fanCirculate' command
+def off() {
+sendEvent (name: "switch", value: "${state.switchLabel1}", descriptionText: "State changed to: ${state.switchLabel1}")
+if (state.qStringParam=="pump") {
+parent.sendPoolCommand("pump","0")
+runIn(2,parent.sendPoolCommand("pump","0"))
+} else {
+parent.sendPoolCommand("${state.qStringParam}","${state.switchValue1}")
+}
+//parent.TimerElapsed()
 }
 
-def setThermostatFanMode() {
-	log.debug "Executing 'setThermostatFanMode'"
-	// TODO: handle 'setThermostatFanMode' command
-}
-
-def setHeatingSetpoint() {
-	log.debug "Executing 'setHeatingSetpoint'"
-	// TODO: handle 'setHeatingSetpoint' command
-}
-
-def open() {
-	log.debug "Executing 'open'"
-	// TODO: handle 'open' command
-}
-
-def close() {
-	log.debug "Executing 'close'"
-	// TODO: handle 'close' command
+def on() {
+sendEvent (name: "switch", value: "${state.switchLabel2}", descriptionText: "State changed to: ${state.switchLabel2}")
+parent.sendPoolCommand("${state.qStringParam}","${state.switchValue2}")
+//parent.TimerElapsed()
 }
